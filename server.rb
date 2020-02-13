0b684db0d90ef13cfb04eec63a5142e661fce7a6
dee-see <dominic.couture@gmail.com>
2020-02-13 09:34:13 -0500

    keep original request's query string params

--- server.rb
+++ server.rb
@@ -8,6 +8,6 @@ post '/*' do
   halt 400 unless host
 
   path = request.path
-  query = JSON.parse(request.body.read)
+  query = JSON.parse(request.body.read, symbolize_names: true).merge(request.params)
   HTTParty.get("#{host}#{path}", headers: { cookie: request.env['HTTP_COOKIE'] }, query: query, debug_output: STDOUT).body
 end
